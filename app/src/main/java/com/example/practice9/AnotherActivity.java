9c669cda8da80f0d8936eb2c812f24a2290c6736
zenghuazhe <570421628@qq.com>
2016-09-09 18:20:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/practice9/AnotherActivity.java
@@ -0,0 +1,29 @@
+package com.example.practice9;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+        Button button=(Button)findViewById(R.id.buttonReturn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AnotherActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+}
+
+
